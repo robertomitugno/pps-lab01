cf2d494de3bf2a550c2c7d80303ea29c21f6c384
robertomitugno <roberto.mitugno@studio.unibo.it>
2025-02-26 23:54:49 +0100

    test: add test for the isLocked() method

---
+++ tdd-exercise/src/main/java/tdd/SmartDoor.java
@@ -0,0 +1,46 @@
+package tdd;
+
+public class SmartDoor implements SmartDoorLock {
+
+    private boolean doorLocked = false;
+
+    @Override
+    public void setPin(int pin) {
+
+    }
+
+    @Override
+    public void unlock(int pin) {
+
+    }
+
+    @Override
+    public void lock() {
+
+    }
+
+    @Override
+    public boolean isLocked() {
+        return doorLocked;
+    }
+
+    @Override
+    public boolean isBlocked() {
+        return false;
+    }
+
+    @Override
+    public int getMaxAttempts() {
+        return 0;
+    }
+
+    @Override
+    public int getFailedAttempts() {
+        return 0;
+    }
+
+    @Override
+    public void reset() {
+
+    }
+}
--- tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
+++ tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
@@ -5,8 +5,11 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SmartDoorLockTest {
+
     @Test
-    public void todo() {
-        assertTrue(true);
+    public void testDoorLock() {
+        SmartDoor smartDoor = new SmartDoor();
+        assertFalse(smartDoor.isLocked());
     }
+
 }
