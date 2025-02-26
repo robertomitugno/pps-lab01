ff4a94cd4c99fa05a103c7e95fcb16d7d445f52a
robertomitugno <roberto.mitugno@studio.unibo.it>
2025-02-27 00:40:23 +0100

    refactor: removed magic number and added beforeEach method to create SmartDoor

--- tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
+++ tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
@@ -1,21 +1,30 @@
 package tdd;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SmartDoorLockTest {
 
+    private static final int PIN_DOOR = 1234;
+    SmartDoor smartDoor;
+
+    @BeforeEach
+    void beforeEach(){
+        smartDoor = new SmartDoor();
+    }
+
     @Test
     public void testLockInitial() {
-        SmartDoor smartDoor = new SmartDoor();
+        smartDoor = new SmartDoor();
         assertFalse(smartDoor.isLocked());
     }
 
     @Test
     public void testLock(){
-        SmartDoor smartDoor = new SmartDoor();
-        smartDoor.setPin(1234);
+        smartDoor = new SmartDoor();
+        smartDoor.setPin(PIN_DOOR);
         smartDoor.lock();
         assertTrue(smartDoor.isLocked());
     }
