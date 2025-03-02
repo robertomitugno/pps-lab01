55e9ce73b98c4db7764e9d6ae48b3488845167d5
RobertoMitugno <roberto.mitugno@studio.unibo.it>
2025-03-02 16:01:20 +0100

    test: add test checking stack initial empty

---
+++ tdd-exercise/src/main/java/tdd/MinMaxStackImpl.java
@@ -0,0 +1,48 @@
+package tdd;
+
+import java.util.Stack;
+
+public class MinMaxStackImpl implements MinMaxStack {
+
+    Stack<Integer> stack;
+
+    MinMaxStackImpl() {
+        stack = new Stack<>();
+    }
+
+    @Override
+    public void push(int value) {
+
+    }
+
+    @Override
+    public int pop() {
+        return 0;
+    }
+
+    @Override
+    public int peek() {
+        return 0;
+    }
+
+    @Override
+    public int getMin() {
+        return 0;
+    }
+
+    @Override
+    public int getMax() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.stack.isEmpty();
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+}
--- tdd-exercise/src/test/java/tdd/MinMaxStackImplTest.java
+++ tdd-exercise/src/test/java/tdd/MinMaxStackImplTest.java
@@ -2,11 +2,13 @@ package tdd;
 
 import org.junit.jupiter.api.Test;
 
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MinMaxStackImplTest {
     @Test
-    public void todo() {
-        assertTrue(true);
+    public void testStackInitialEmpty() {
+        MinMaxStackImpl minMaxStack = new MinMaxStackImpl();
+        assertTrue(minMaxStack.isEmpty());
     }
 }
